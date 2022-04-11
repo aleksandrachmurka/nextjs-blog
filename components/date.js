d8535b72f010616201b3112fb790bea56980cc10
Aleksandra Krzywicka-Guz <aleksandrakrzywicka@gmail.com>
2022-04-11 12:37:53 +0200

    Add blog

---
+++ components/date.js
@@ -0,0 +1,6 @@
+import { parseISO, fornat, format } from 'date-fns'
+
+export default function Date({ dateString }) {
+  const date = parseISO(dateString)
+  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
+}
